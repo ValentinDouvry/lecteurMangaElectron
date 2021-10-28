ccf6c7116c4d4cb1f9e8276f0ddde5df061ecda2
David <dadahuet13@gmail.com>
2021-10-28 21:55:30 +0400

    v1 account

--- src/actions/account.js
+++ src/actions/account.js
@@ -1,22 +1,35 @@
-const usernameMenu = document.getElementById("usernameMenu");
-const profilImageMenu = document.getElementById("profilImageMenu");
 const favorisMenu = document.getElementById("favorisMenu");
+const profileImage = document.getElementById("profileImage");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const inputFile = document.getElementById("inputFile");
+
 
 main();
 
 async function main() {
   const user = await getUser(2);
 
-  usernameMenu.innerText = user.username;
-  usernameMenu.href = `acount.html?id=${user.id}`;
-  profilImageMenu.src = user.image_path;
+  console.log(user);
+
   favorisMenu.href = `favorites.html?id=${user.id}`;
+  profileImage.src = user.image_path;
+  username.value = user.username;
+  email.value = user.email;
+
+  inputFile.addEventListener("change", (e) => {
+    profileImage.src = URL.createObjectURL(e.target.files[0]);
+    
+    profileImage.onload = function() {
+      URL.revokeObjectURL(profileImage.src) // free memory
+    }
+  });
 }
 
 
 async function getUser(id) {
   try {
-    const response = await axios.get(`localhost:8080/lecteurManga/rest/accounts/${id}`);
+    const response = await axios.get(`http://133a-154-67-229-122.ngrok.io/lecteurManga/rest/accounts/${id}`);
     return response.data;
   } catch (error) {
     console.error(error);
